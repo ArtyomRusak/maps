1ee6869c1e776922576ff465c0bb8f2d2498532c
ArtsiomRusak <artsiom.rusak@outlook.com>
2015-02-15 16:26:40 +0200

    [Add]: Initial commit. [ArtsiomRusak]

---
+++ ArcgisMapsTest/ArcgisMapsTest/Controllers/MapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArcgisMapsTest.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: Map
+        public ActionResult ViewMap()
+        {
+            return View();
+        }
+    }
+}
---
+++ ArcgisMapsTest/ArcgisMapsTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArcgisMapsTest.MvcApplication" Language="C#" %>
