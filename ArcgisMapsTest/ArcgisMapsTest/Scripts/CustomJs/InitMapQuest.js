041f4ae50382e0195a57cb978a58d3b18d48e4b0
ArtsiomRusak <bembi1204@gmail.com>
2015-03-07 15:43:13 +0200

    [Update]: Start implementation of selecting points. [ArtsiomRusak]

--- ArcgisMapsTest/ArcgisMapsTest/Scripts/CustomJs/InitMapQuest.js
+++ ArcgisMapsTest/ArcgisMapsTest/Scripts/CustomJs/InitMapQuest.js
@@ -1,4 +1,9 @@
-﻿$(function () {
+﻿var draw = false;
+var rectangleForChoose;
+var mapDiv;
+var offx, offy, map;
+
+$(function () {
   var options = {
     elt: $("#map")[0],
     zoom: 5,
@@ -7,103 +12,161 @@
     zoomOnDoubleCheck: true
   };
 
-  var map = new MQA.TileMap(options);
-  var drawingOverlay = new MQA.DrawingOverlay();
+  draw = false;
+  mapDiv = $("#map")[0];
+  offx = mapDiv.offsetLeft - mapDiv.scrollLeft;
+  offy = mapDiv.offsetTop - mapDiv.scrollTop;
+  map = new MQA.TileMap(options);
+  MQA.withModule('shapes', function () { });
+});
 
-  MQA.withModule('largezoom', 'mousewheel', function () {
-    map.addControl(
-      new MQA.LargeZoom(),
-      new MQA.MapCornerPlacement(MQA.MapCorner.TOP_LEFT, new MQA.Size(5, 5))
-    );
+var suppressor = document.createElement('div'); // create a div to block map events, but don't use it until you want to drawr
+suppressor.id = "stopit";
+suppressor.style.position = "absolute";
+suppressor.style.width = "100%";
+suppressor.style.height = "100%";
+suppressor.style.top = "0px";
+suppressor.style.left = "0px";
+suppressor.style.zIndex = "200";
+suppressor.style.backgroundImage = "url(http://developer.mapquest.com/DevNet-Rebrand2-theme/html/themes/mq_theme/images/spacer.gif)";
+suppressor.onmousedown = suppressorhandler;
 
-    map.enableMouseWheelZoom();
-  });
+function cursorPosition(evt) { // get cursor offsets
+  var x = (MQA.Util.getBrowserInfo().name == "msie") ? window.event.clientX - offx : evt.pageX - offx;
+  var y = (MQA.Util.getBrowserInfo().name == "msie") ? window.event.clientY - offy : evt.pageY - offy;// funky if scrolling in IE
+  return map.pixToLL(new MQA.Point(x, y));
+}
 
-  $("#addMarkerBtn").click(function () {
-    var poi = new MQA.Poi({ lat: $("#xCoor").val(), lng: $("#yCoor").val() });
-    poi.setDeclutterMode(true);
-    poi.myInfo = "Hello from " + $("#xCoor").val() + " " + $("#yCoor").val();
-    poi.draggable = true;
+function suppressorhandler(e) {
+  var cursorll1 = cursorPosition(e);
+  rectangleForChoose = new MQA.RectangleOverlay();
+  rectangleForChoose.borderWidth = 1;
+  rectangleForChoose.fillColorAlpha = 0.5;
+  rectangleForChoose.fillColor = "#00ff00";
 
-    poi.setRolloverContent(poi.myInfo);
+  rectangleForChoose.shapePoints = [cursorll1.lat, cursorll1.lng];
+  suppressor.onmousemove = function (evt) {
+    var cursorll2 = cursorPosition(evt);
+    rectangleForChoose.setShapePoints([
+      /*
+      make sure to get upper left and lower right points set correctly.
+      */
+      cursorll1.lat > cursorll2.lat ? cursorll1.lat : cursorll2.lat,
+      cursorll1.lng < cursorll2.lng ? cursorll1.lng : cursorll2.lng,
+      cursorll1.lat < cursorll2.lat ? cursorll1.lat : cursorll2.lat,
+      cursorll1.lng > cursorll2.lng ? cursorll1.lng : cursorll2.lng
+    ]);
 
-    map.addShape(poi);
-  });
+    map.addShape(rectangleForChoose);
+    suppressor.onmouseup = function (evt) {
+      suppressor.onmousemove = null;
+      suppressor.onmouseup = null;
+      mapDiv.removeChild(suppressor);
+      mapDiv.appendChild(suppressor);
+    }
+  }
+}
 
-  $("#selectMarkers").click(function () {
+MQA.withModule('largezoom', 'mousewheel', function () {
+  map.addControl(
+    new MQA.LargeZoom(),
+    new MQA.MapCornerPlacement(MQA.MapCorner.TOP_LEFT, new MQA.Size(5, 5))
+  );
 
-  });
+  map.enableMouseWheelZoom();
+});
 
-  $("#AddMarkerClickBtn").click(function() {
-    MQA.withModule('new-route', function() {
-      var opt = {
-        request: {
-          locations: ['Gunnison, CO', 'Ouray, CO'],
+$("#addMarkerBtn").click(function () {
+  var poi = new MQA.Poi({ lat: $("#xCoor").val(), lng: $("#yCoor").val() });
+  poi.setDeclutterMode(true);
+  poi.myInfo = "Hello from " + $("#xCoor").val() + " " + $("#yCoor").val();
+  poi.draggable = true;
 
-          options: {
-            avoids: [],
-            avoidTimedConditions: false,
-            doReverseGeocode: true,
-            shapeFormat: 'raw',
-            generalize: 0,
-            routeType: 'fastest',
-            timeType: 1,
-            locale: 'ru_RU',
-            unit: 'm',
-            enhancedNarrative: false,
-            drivingStyle: 2,
-            highwayEfficiency: 21.0
-          }
-        },
+  poi.setRolloverContent(poi.myInfo);
 
-        display: {
-          color: '#800000',
-          borderWidth: 10,
-          draggablePoi: true
-        },
+  map.addShape(poi);
+});
 
-        // on success, display the route narrative
-        success: function displayNarrative(data) {
-          if (data.route) {
-            var legs = data.route.legs,
-              html = '',
-              i = 0,
-              j = 0,
-              trek,
-              maneuver;
+$("#selectMarkers").click(function (btn) {
+  if (draw) {
+    mapDiv.removeChild(suppressor);
+    btn.value = 'DRAW!';
+  } else {
+    mapDiv.appendChild(suppressor);
+    btn.value = 'STOP!';
+  }
+  draw = !draw;
+});
 
-            html += '<table class="clean"><tbody>';
+$("#AddMarkerClickBtn").click(function () {
+  MQA.withModule('new-route', function () {
+    var opt = {
+      request: {
+        locations: ['Gunnison, CO', 'Ouray, CO'],
 
-            for (; i < legs.length; i++) {
-              for (j = 0; j < legs[i].maneuvers.length; j++) {
-                maneuver = legs[i].maneuvers[j];
-                html += '<tr>';
-                html += '<td>';
-
-                if (maneuver.iconUrl) {
-                  html += '<img src="' + maneuver.iconUrl + '" />';
-                }
-
-                for (k = 0; k < maneuver.signs.length; k++) {
-                  var sign = maneuver.signs[k];
-
-                  if (sign && sign.url) {
-                    html += '<img src="' + sign.url + '" />';
-                  }
-                }
-
-                html += '</td><td>' + maneuver.narrative + '</td>';
-                html += '</tr>';
-              }
-            }
-
-            html += '</tbody></table>';
-            $("#route-results")[0].innerHTML = html;
-          }
+        options: {
+          avoids: [],
+          avoidTimedConditions: false,
+          doReverseGeocode: true,
+          shapeFormat: 'raw',
+          generalize: 0,
+          routeType: 'fastest',
+          timeType: 1,
+          locale: 'ru_RU',
+          unit: 'm',
+          enhancedNarrative: false,
+          drivingStyle: 2,
+          highwayEfficiency: 21.0
         }
-      };
+      },
 
-      map.addRoute(opt);
-    });
+      display: {
+        color: '#800000',
+        borderWidth: 10,
+        draggablePoi: true
+      },
+
+      // on success, display the route narrative
+      success: function displayNarrative(data) {
+        if (data.route) {
+          var legs = data.route.legs,
+            html = '',
+            i = 0,
+            j = 0,
+            trek,
+            maneuver;
+
+          html += '<table class="clean"><tbody>';
+
+          for (; i < legs.length; i++) {
+            for (j = 0; j < legs[i].maneuvers.length; j++) {
+              maneuver = legs[i].maneuvers[j];
+              html += '<tr>';
+              html += '<td>';
+
+              if (maneuver.iconUrl) {
+                html += '<img src="' + maneuver.iconUrl + '" />';
+              }
+
+              for (k = 0; k < maneuver.signs.length; k++) {
+                var sign = maneuver.signs[k];
+
+                if (sign && sign.url) {
+                  html += '<img src="' + sign.url + '" />';
+                }
+              }
+
+              html += '</td><td>' + maneuver.narrative + '</td>';
+              html += '</tr>';
+            }
+          }
+
+          html += '</tbody></table>';
+          $("#route-results")[0].innerHTML = html;
+        }
+      }
+    };
+
+    map.addRoute(opt);
   });
 });
