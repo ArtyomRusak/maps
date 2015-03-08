26437257fc44be6ffc8311cc2b936fa333aca898
ArtsiomRusak <bembi1204@gmail.com>
2015-03-08 14:34:07 +0200

    [Update]: Adding routing. [ArtsiomRusak]

--- ArcgisMapsTest/ArcgisMapsTest/Scripts/CustomJs/InitMapQuest.js
+++ ArcgisMapsTest/ArcgisMapsTest/Scripts/CustomJs/InitMapQuest.js
@@ -3,8 +3,10 @@ var rectangleForChoose;
 var mapDiv;
 var offx, offy, map;
 var points = new Array();
+var selectedPoints = new Array();
 var selectedIcon = new MQA.Icon('http://developer.mapquest.com/content/documentation/common/images/smiley.png', 22, 28);
 var defaultIcon = new MQA.Icon('http://icons.mqcdn.com/icons/stop.png', 22, 28);
+var selectForRoute = false;
 
 
 $(function () {
@@ -21,8 +23,21 @@ $(function () {
   offx = mapDiv.offsetLeft - mapDiv.scrollLeft;
   offy = mapDiv.offsetTop - mapDiv.scrollTop;
   map = new MQA.TileMap(options);
+  MQA.EventManager.addListener(map, 'click', addButtonByClick);
   MQA.withModule('shapes', function () { });
 });
+
+function addButtonByClick(evt) {
+  var poi = new MQA.Poi({ lat: evt.ll.lat, lng: evt.ll.lng });
+
+  poi.myInfo = "Hello from " + evt.ll.lat + " " + evt.ll.lng;
+  //poi.draggable = true;
+
+  poi.setRolloverContent(poi.myInfo);
+
+  map.addShape(poi);
+  points.push(poi);
+};
 
 var suppressor = document.createElement('div'); // create a div to block map events, but don't use it until you want to drawr
 suppressor.id = "stopit";
@@ -49,7 +64,7 @@ function suppressorhandler(e) {
   rectangleForChoose.fillColor = "#00ff00";
 
   rectangleForChoose.shapePoints = [cursorll1.lat, cursorll1.lng];
-  suppressor.onmousemove = function(evt) {
+  suppressor.onmousemove = function (evt) {
     var cursorll2 = cursorPosition(evt);
     rectangleForChoose.setShapePoints([
       /*
@@ -65,30 +80,32 @@ function suppressorhandler(e) {
 
   map.addShape(rectangleForChoose);
 
-    suppressor.onmouseup = function (evt) {
-      suppressor.onmousemove = null;
-      suppressor.onmouseup = null;
-      mapDiv.removeChild(suppressor);
-      mapDiv.appendChild(suppressor);
+  suppressor.onmouseup = function (evt) {
+    suppressor.onmousemove = null;
+    suppressor.onmouseup = null;
+    mapDiv.removeChild(suppressor);
+    mapDiv.appendChild(suppressor);
 
-      for (var i = 0; i < points.length; i++) {
-        //if (points[i]) {
+    selectedPoints = [];
+    for (var i = 0; i < points.length; i++) {
+      //if (points[i]) {
 
-        //}
-        var pointLatLng = points[i].latLng;
-        var shapePoints = rectangleForChoose.getShapePoints();
-        if (pointLatLng.lat < shapePoints[0] && pointLatLng.lat > shapePoints[2] && pointLatLng.lng > shapePoints[1] && pointLatLng.lng < shapePoints[3]) {
-          points[i].setIcon(selectedIcon);
-        } else {
-          points[i].setIcon(defaultIcon);
-        }
-        //points[i].setState("red");
-        var state = points[i].getState();
-        var icon = points[i].getIcon();
-
-        map.removeShape(rectangleForChoose);
+      //}
+      var pointLatLng = points[i].latLng;
+      var shapePoints = rectangleForChoose.getShapePoints();
+      if (pointLatLng.lat < shapePoints[0] && pointLatLng.lat > shapePoints[2] && pointLatLng.lng > shapePoints[1] && pointLatLng.lng < shapePoints[3]) {
+        points[i].setIcon(selectedIcon);
+        selectedPoints.push(points[i]);
+      } else {
+        points[i].setIcon(defaultIcon);
       }
+      //points[i].setState("red");
+      var state = points[i].getState();
+      var icon = points[i].getIcon();
+
+      map.removeShape(rectangleForChoose);
     }
+  }
 }
 
 MQA.withModule('largezoom', 'mousewheel', function () {
@@ -121,6 +138,23 @@ $("#selectMarkers").click(function (btn) {
     btn.target.value = 'Stop';
   }
   draw = !draw;
+});
+
+$("#selectMarkersForRoute").click(function (btn) {
+  MQA.withModule('new-route', function () {
+    var location = [];
+    for (var i = 0; i < selectedPoints.length; i++) {
+      location.push({ latLng: { lat: selectedPoints[i].latLng.lat, lng: selectedPoints[i].latLng.lng } });
+    }
+
+    if (location.length !== 0) {
+      map.addOptimizedRoute({
+        request: {
+          locations: location
+        }
+      });
+    }
+  });
 });
 
 $("#AddMarkerClickBtn").click(function () {
